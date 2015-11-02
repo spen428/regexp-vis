2bf1b69b735822c0110f2a4c2c629ae2a959d318
M.J.Nicholls <mjn33@kent.ac.uk>
2015-11-02 11:43:54 +0000

    Add basic skeleton of main project, uses Eclipse
    Only has empty classes at the moment, Eclipse is being used for
    now. Migration to using Ant as well should come later.

---
+++ regexp_vis/src/main/Program.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * @author mjn33
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ regexp_vis/src/model/BasicRegexp.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * @author Matthew Nicholls
+ *
+ * Class representing a "basic" regular expression, with one top level
+ * operator. A tree of these can then be used to any regular
+ * expression
+ */
+public class BasicRegexp {
+
+    public BasicRegexp()
+    {
+    }
+}
