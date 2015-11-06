5e6ae37f4b99ee67d5b79d350d4b84c9b790326e
S.J.Pengelly <sp611@kent.ac.uk>
2015-11-06 13:58:03 +0000

    Added comments

--- regexp_vis/src/test/ui/GraphicsTest.java
+++ regexp_vis/src/test/ui/GraphicsTest.java
@@ -8,63 +8,53 @@ import javax.swing.JFrame;
 
 import ui.Graph;
 import ui.GraphPanel;
+import ui.GraphPanel.GraphLayout;
 
 /**
- * Demonstrates the capabilities of JGraphX.
+ * Demonstrates the graph rendering capabilities of JGraphX.
  * 
  * @author sp611
  * 
  */
 public class GraphicsTest extends KeyAdapter implements KeyListener {
 
-	/**
-	 * Default diameter of vertices.
-	 */
 	private static final int VERTEX_DIAMETER_PX = 50;
-	private static final int JFRAME_WIDTH_PX = 450;
-	private static final int JFRAME_HEIGHT_PX = 450;
+	private static final int NUM_VERTICES = 10;
+	private static final int JFRAME_WIDTH_PX = VERTEX_DIAMETER_PX * NUM_VERTICES;
+	private static final int JFRAME_HEIGHT_PX = JFRAME_WIDTH_PX;
+
 	private final JFrame frame;
 	private final GraphPanel graphPanel;
 
-	public GraphicsTest(Graph graph) {
+	public GraphicsTest() {
+		/* Construct the JFrame */
 		frame = new JFrame("Regular Language Visualiser - "
 				+ "Graph Layout Demo");
 		frame.addKeyListener(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(JFRAME_WIDTH_PX, JFRAME_HEIGHT_PX);
-		graphPanel = new GraphPanel(graph);
+
+		/* Instantiate GraphPanel, providing the test graph generated below. */
+		graphPanel = new GraphPanel(generateTestGraph());
 		graphPanel.addKeyListener(this);
 		frame.getContentPane().add(graphPanel);
 		frame.setVisible(true);
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		char keyChar = e.getKeyChar();
-		switch (keyChar) {
-		default:
-		case 'a':
-			graphPanel.setVertexLayout(0);
-			break;
-		case 's':
-			graphPanel.setVertexLayout(1);
-			break;
-		case 'd':
-			graphPanel.setVertexLayout(2);
-			break;
-		}
-	}
-
-	public static void main(String[] args) {
-		Graph graph = makeGraph();
-		new GraphicsTest(graph);
-	}
-
-	private static Graph makeGraph() {
+	/**
+	 * Generates a simple cyclic graph with two edges per vertex.
+	 * 
+	 * @return The generated {@link Graph}
+	 */
+	private static Graph generateTestGraph() {
 		Graph graph = new Graph();
 		Object parent = graph.getDefaultParent();
 
 		graph.getModel().beginUpdate();
+		/*
+		 * A try-finally block is recommended to ensure that endUpdate() is
+		 * always called even if an exception is thrown.
+		 */
 		try {
 			/* Add some vertices and edges, all at position (0,0). */
 			Object vFirst, vPrev, vNew;
@@ -72,21 +62,54 @@ public class GraphicsTest extends KeyAdapter implements KeyListener {
 					VERTEX_DIAMETER_PX, VERTEX_DIAMETER_PX);
 			vPrev = vFirst;
 
-			for (int i = 0; i <= 9; i++) {
+			/* Loop to generate vertices and connect them all together. */
+			char edgeLabel;
+			for (int i = 0; i < NUM_VERTICES; i++) {
 				vNew = graph.insertVertex(parent, null, (i + 1), 0, 0,
 						VERTEX_DIAMETER_PX, VERTEX_DIAMETER_PX);
-				graph.insertEdge(parent, null, (char) ('A' + i), vPrev, vNew);
-				graph.insertEdge(parent, null, (char) ('A' + (2 * (i + 1))),
-						vPrev, vNew);
+
+				edgeLabel = (char) ('A' + i);
+				graph.insertEdge(parent, null, edgeLabel, vPrev, vNew);
+
+				edgeLabel = (char) ('A' + (2 * (i + 1)));
+				graph.insertEdge(parent, null, edgeLabel, vNew, vPrev);
+
+				/*
+				 * Keep track of previously generated vertices, so that they can
+				 * be connected together in the loop above.
+				 */
 				vPrev = vNew;
 			}
 
+			/* Finally, connect the first and last vertices together. */
 			graph.insertEdge(parent, null, (char) ('Y'), vPrev, vFirst);
-			graph.insertEdge(parent, null, (char) ('Z'), vPrev, vFirst);
+			graph.insertEdge(parent, null, (char) ('Z'), vFirst, vPrev);
 		} finally {
 			graph.getModel().endUpdate();
 		}
 		return graph;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		/* Switch between vertex layouts with the keyboard. */
+		char keyChar = e.getKeyChar();
+		switch (keyChar) {
+		default:
+		case 'a':
+			graphPanel.setVertexLayout(GraphLayout.CIRCLE_LAYOUT);
+			break;
+		case 's':
+			graphPanel.setVertexLayout(GraphLayout.ORGANIC_LAYOUT);
+			break;
+		case 'd':
+			graphPanel.setVertexLayout(GraphLayout.FAST_ORGANIC_LAYOUT);
+			break;
+		}
+	}
+
+	public static void main(String[] args) {
+		new GraphicsTest();
 	}
 
 }
--- regexp_vis/src/ui/GraphPanel.java
+++ regexp_vis/src/ui/GraphPanel.java
@@ -25,22 +25,38 @@ import com.mxgraph.view.mxGraph;
  */
 public class GraphPanel extends mxGraphComponent {
 
+	public enum GraphLayout {
+		CIRCLE_LAYOUT, ORGANIC_LAYOUT, FAST_ORGANIC_LAYOUT;
+	}
+
 	private final mxGraph graph;
 	private final mxGraphLayout vertexCircleLayout, vertexOrganicLayout,
 			vertexFastOrganicLayout, edgeLayout, edgeLabelLayout;
 
+	/**
+	 * Creates a new instance of {@link GraphPanel} and attaches a new instance
+	 * of {@link Graph} to it.
+	 */
 	public GraphPanel() {
 		this(new Graph());
 	}
 
+	/**
+	 * Creates a new instance of {@link GraphPanel} and attaches the given
+	 * {@link Graph} to it.
+	 * 
+	 * @param graph
+	 *            the {@link Graph} to attach to this {@link GraphPanel}.
+	 */
 	public GraphPanel(Graph graph) {
 		super(graph);
 		this.graph = graph;
 
 		/*
-		 * The layouts used may vary depending on the structure of the graph. We
-		 * will need to throw a bunch of different graph structures at it and
-		 * see.
+		 * A number of different vertex layout managers, that have their own
+		 * benefits. It may be necessary to switch between them for certain
+		 * graph structures, so I have provided that functionality with the
+		 * setVertexLayout() method.
 		 */
 		this.vertexCircleLayout = new mxCircleLayout(graph);
 		this.vertexOrganicLayout = new mxOrganicLayout(graph);
@@ -48,23 +64,35 @@ public class GraphPanel extends mxGraphComponent {
 		this.edgeLayout = new mxParallelEdgeLayout(graph);
 		this.edgeLabelLayout = new mxEdgeLabelLayout(graph);
 
-		setVertexLayout(0);
+		setVertexLayout(GraphLayout.CIRCLE_LAYOUT);
 	}
 
-	public void setVertexLayout(int layout) {
+	/**
+	 * Switches between the automatic vertex layout types provided by JGraphX.
+	 * This method automatically calls the <code>execute()</code> method on the
+	 * chosen {@link mxGraphLayout} type, and also updates the positions of
+	 * edges and edge labels after the vertices have been laid-out.
+	 * 
+	 * @param layout
+	 *            the {@link GraphPanel.GraphLayout} to use.
+	 */
+	public void setVertexLayout(GraphLayout layout) {
 		Object parent = graph.getDefaultParent();
+
 		switch (layout) {
 		default:
-		case 0:
+		case CIRCLE_LAYOUT:
 			vertexCircleLayout.execute(parent);
 			break;
-		case 1:
+		case FAST_ORGANIC_LAYOUT:
 			vertexFastOrganicLayout.execute(parent);
 			break;
-		case 2:
+		case ORGANIC_LAYOUT:
 			vertexOrganicLayout.execute(parent);
 			break;
 		}
+
+		/* Update edges and edge labels now that the vertices have moved. */
 		edgeLayout.execute(parent);
 		edgeLabelLayout.execute(parent);
 	}
