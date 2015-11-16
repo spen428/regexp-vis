0904d9374d1477d1511fa03d41f36d549691d675
S.J.Pengelly <sp611@kent.ac.uk>
2015-11-16 14:01:36 +0000

    Added test suites for conveniently running multiple tests.

---
+++ regexp_vis/src/test/model/AllModelTests.java
@@ -0,0 +1,10 @@
+package test.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CommandHistoryTest.class })
+public class AllModelTests {
+}
---
+++ regexp_vis/src/test/ui/AllUITests.java
@@ -0,0 +1,10 @@
+package test.ui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UICommandHistoryTest.class })
+public class AllUITests {
+}
