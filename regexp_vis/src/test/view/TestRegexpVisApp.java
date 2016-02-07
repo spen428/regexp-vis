15946bf2b064941060b0d3d11324ac31e34c10b1
S.J.Pengelly <sp611@kent.ac.uk>
2016-02-07 19:16:28 +0000

    auto breakdown of regexp for position testing

---
+++ regexp_vis/src/test/view/TestRegexpVisApp.java
@@ -0,0 +1,38 @@
+package test.view;
+
+import controller.RegexpVisApp;
+import javafx.stage.Stage;
+import view.Activity;
+import view.GraphCanvasEvent;
+import view.GraphCanvasFX;
+
+/**
+ * Extension of {@link RegexpVisApp} with exposed methods for testing purposes.s
+ * 
+ * @author sp611
+ *
+ */
+public class TestRegexpVisApp extends RegexpVisApp {
+
+    public TestRegexpVisApp(Stage stage) {
+        super(stage);
+    }
+
+    public TestRegexpVisApp(Stage stage, String regexp) {
+        super(stage);
+        this.currentActivity.onEnteredRegexp(regexp);
+    }
+
+    public void setActivity(Activity<GraphCanvasEvent> activity) {
+        this.currentActivity = activity;
+    }
+
+    public Activity<GraphCanvasEvent> getCurrentActivity() {
+        return this.currentActivity;
+    }
+
+    public GraphCanvasFX getGraph() {
+        return this.mCanvas;
+    }
+
+}
