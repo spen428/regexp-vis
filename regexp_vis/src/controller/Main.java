d72ae57902c2839384e3c8e0230f31e40358944e
M.J.Nicholls <mjn33@kent.ac.uk>
2016-02-08 13:52:04 +0000

    Correct name and version of the application

--- regexp_vis/src/controller/Main.java
+++ regexp_vis/src/controller/Main.java
@@ -5,8 +5,9 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static final String TITLE = "Regular Expression Visualiser";
-    public static final String VERSION = "0.987a";
+    public static final String TITLE = "Regular Language Visualiser";
+    // 0.{iteration number} alpha, 1.0 when feature complete
+    public static final String VERSION = "0.3a";
 
     public static void main(String[] args) {
         Application.launch(args);
