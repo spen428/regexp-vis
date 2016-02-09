540f64c99d3e2826b30e450bcdaefe91fb506480
S.J.Pengelly <sp611@kent.ac.uk>
2016-02-09 12:36:15 +0000

    test iteration after choice positioning

---
+++ regexp_vis/src/test/controller/BreakdownIterationAfterChoicePositioningTest.java
@@ -0,0 +1,18 @@
+package test.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BreakdownIterationAfterChoicePositioningTest extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        TestRegexpVisApp ui = new TestRegexpVisApp(stage, "b*|d*");
+        BreakdownTestUtils.breakdownEdges(ui, 0, 1);
+    }
+
+}
