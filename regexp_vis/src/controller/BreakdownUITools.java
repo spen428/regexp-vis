eb0be29097873d83e5c1ac6f1fcf942e6343930f
S.J.Pengelly <sp611@kent.ac.uk>
2016-03-10 13:22:30 +0000

    Cleaned up code in BreakdownUITools, removing deprecated and unused methods. Implemented varied node placement, so that nodes rarely appear directly ontop of eachother

--- regexp_vis/src/controller/BreakdownUITools.java
+++ regexp_vis/src/controller/BreakdownUITools.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -11,8 +12,6 @@ import model.Automaton;
 import model.AutomatonState;
 import model.AutomatonTransition;
 import model.BreakdownCommand;
-import model.BreakdownIterationCommand;
-import model.BreakdownSequenceCommand;
 import model.Command;
 import model.RemoveStateCommand;
 import view.GraphCanvasFX;
@@ -26,91 +25,9 @@ public class BreakdownUITools {
 
     private static final Logger LOGGER = Logger.getLogger("controller");
 
-    public static final double MIN_BREAKDOWN_HEIGHT_PX = 4
-            * GraphCanvasFX.DEFAULT_NODE_RADIUS;
+    public static final double MIN_BREAKDOWN_HEIGHT = 4 * GraphCanvasFX.DEFAULT_NODE_RADIUS;
 
-    private static Point2D[] computeRectanglePoints(double ax, double ay,
-            double bx, double by, double h, boolean above) {
-        final int sign = above ? 1 : -1;
-        final double deltaX = bx - ax;
-        final double deltaY = by - ay;
-        final double theta = Math.atan(deltaY / deltaX);
-        final double w = h * Math.sin(theta) * sign;
-        final double n = Math.sqrt(h * h - w * w) * sign;
-        final double cx = (ax + w);
-        final double cy = (ay - n);
-        final double dx = (bx + w);
-        final double dy = (by - n);
-        return new Point2D[] { new Point2D(cx, cy), new Point2D(dx, dy) };
-    }
-
-    /**
-     * Calculate the third point of triangle ABC, where points A and B lie on
-     * the base of the triangle.
-     *
-     * @param ax
-     *            x coordinate of A
-     * @param ay
-     *            y coordinate of A
-     * @param bx
-     *            x coordinate of B
-     * @param by
-     *            y coordinate of B
-     * @param h
-     *            the height of the triangle
-     * @param above
-     *            whether to find the y > midY or the y < midY point
-     * @return {@link Point2D} C
-     */
-    private static Point2D computeThirdPoint(double ax, double ay, double bx,
-            double by, double h, boolean above) {
-        final int sign = above ? 1 : -1;
-        final double dx = bx - ax;
-        final double dy = by - ay;
-        final double theta = Math.atan(dy / dx); // Slope of line AB
-        final double mx = ax + (dx / 2); // Midpoint
-        final double my = ay + (dy / 2);
-        final double w = h * Math.sin(theta) * sign;
-        final double n = Math.sqrt(h * h - w * w) * sign;
-        final double cx = (mx + w);
-        final double cy = (my - n);
-        return new Point2D(cx, cy);
-    }
-
-    /**
-     * Iterate
-     * {@link #computeThirdPoint(double, double, double, double, boolean)} over
-     * a set of points.
-     */
-    private static Point2D[] computeTriangles(double ax, double ay, double bx,
-            double by, int numPoints, double height, boolean above) {
-        if (numPoints < 1) {
-            throw new IllegalArgumentException();
-        }
-
-        final double deltaX = bx - ax;
-        final double deltaY = by - ay;
-        final double dxPerNode = deltaX / (numPoints + 1);
-        final double dyPerNode = deltaY / (numPoints + 1);
-
-        double prevX = ax;
-        double prevY = ay;
-        double curX = ax + 2 * dxPerNode;
-        double curY = ay + 2 * dyPerNode;
-
-        Point2D[] points = new Point2D[numPoints];
-
-        for (int i = 0; i < numPoints; i++) {
-            points[i] = computeThirdPoint(prevX, prevY, curX, curY, height,
-                    above);
-            prevX += dxPerNode;
-            prevY += dyPerNode;
-            curX += dxPerNode;
-            curY += dyPerNode;
-        }
-
-        return points;
-    }
+    private static final double VARIATION = 10;
 
     /**
      * @return The number of occurrences of {@link AddStateCommand} in the given
@@ -185,77 +102,65 @@ public class BreakdownUITools {
     }
 
     /**
-     * Intelligently place the nodes created by the given
-     * {@link BreakdownCommand} along a line or curve between the two existing
-     * nodes from which this {@link BreakdownCommand} was generated.
-     *
-     * @param graph
-     *            the {@link GraphCanvasFX}
+     * Returns an array of the new {@link AutomatonTransition}s added by this
+     * {@link BreakdownUICommand}
+     * 
      * @param cmd
-     *            the {@link BreakdownCommand}
-     * @return a {@link Point2D} array of the locations to place the new nodes,
-     *         in order starting from the "from" end.
+     *            the {@link BreakdownUICommand}
+     * @return an array of {@link AutomatonTransition}
      */
-    @Deprecated
-    public static Point2D[] placeNodesOld(GraphCanvasFX graph,
-            BreakdownCommand cmd) {
-        final AutomatonTransition transition = cmd.getOriginalTransition();
-        final AutomatonState fromState = transition.getFrom();
-        final AutomatonState toState = transition.getTo();
-        final int numAddedStates = BreakdownUITools.getNumAddedStates(cmd);
-        final boolean fromChoice = BreakdownUITools.wasChoiceTransition(cmd);
-        final double fromX = graph.lookupNode(fromState.getId()).getX();
-        final double fromY = graph.lookupNode(fromState.getId()).getY();
-        final double toX = graph.lookupNode(toState.getId()).getX();
-        final double toY = graph.lookupNode(toState.getId()).getY();
-        final double dx = toX - fromX;
-        final double dy = toY - fromY;
-        final double len = Math.sqrt(dx * dx + dy * dy);
-
-        Point2D[] points = new Point2D[numAddedStates];
-        if (numAddedStates > 0) {
-            boolean above = (numAddedStates == 1); // Arbitrary default value
-            double height = MIN_BREAKDOWN_HEIGHT_PX;
-
-            if (fromChoice) {
-                Point2D edgeMid = graph
-                        .lookupEdge(cmd.getOriginalTransition().getId())
-                        .getEdgeMiddlePoint();
-
-                above = (edgeMid.getY() > dy);
-                double mx = fromX + dx / 2;
-                double my = fromY + dy / 2;
-                double ex = edgeMid.getX() - mx;
-                double ey = edgeMid.getY() - my;
-                height += Math.sqrt(ex * ex + ey * ey);
-            } else if (cmd instanceof BreakdownSequenceCommand) {
-                height = 0;
-            }
-
-            if (cmd instanceof BreakdownIterationCommand && numAddedStates == 2
-                    && len <= MIN_BREAKDOWN_HEIGHT_PX * 2) {
-                /* A bit too cramped, make a rectangle instead */
-                points = computeRectanglePoints(fromX, fromY, toX, toY, height,
-                        above);
-            } else {
-                points = computeTriangles(fromX, fromY, toX, toY,
-                        numAddedStates, height, above);
+    public static AutomatonTransition[] getNewTransitions(BreakdownUICommand cmd) {
+        ArrayList<AutomatonTransition> transitions = new ArrayList<>();
+        for (UICommand c : cmd.getCommands()) {
+            if (c instanceof AddTransitionUICommand) {
+                transitions.add(((AddTransitionUICommand) c).getTransition());
             }
         }
-        return points;
+        return transitions.toArray(new AutomatonTransition[0]);
     }
 
-    public static Point2D[] placeNodes(GraphCanvasFX canvas,
-            BreakdownCommand cmd) {
+    /**
+     * @return A random {@link Point2D} located between (0,0) and (800,600)
+     */
+    public static Point2D randomPoint() {
+        return new Point2D(Math.random() * 800, Math.random() * 600);
+    }
+
+    /**
+     * @return A new {@link Point2D} located at (x,y) plus-or-minus
+     *         {@value #VARIATION}
+     */
+    private static Point2D variedPoint(double x, double y) {
+        double dx = Math.random() * VARIATION;
+        double dy = Math.random() * VARIATION;
+        return new Point2D(x + dx, y + dy);
+    }
+
+    /**
+     * Intelligently place new {@link AutomatonState}s that are created by this
+     * {@link BreakdownCommand}, so that the layout requires minimal alteration
+     * to be readable
+     * 
+     * @param graph
+     *            the {@link GraphCanvasFX} onto which the
+     *            {@link AutomatonState}s will be rendered
+     * @param cmd
+     *            the {@link BreakdownCommand}
+     * @return an array of {@link Point2D} containing the intended location for
+     *         each of the new {@link AutomatonState}, in the order that their
+     *         {@link AddStateCommand}s appear when iterating over the
+     *         {@link BreakdownCommand}
+     */
+    public static Point2D[] placeNodes(GraphCanvasFX graph, BreakdownCommand cmd) {
         final AutomatonTransition transition = cmd.getOriginalTransition();
         final AutomatonState fromState = transition.getFrom();
         final AutomatonState toState = transition.getTo();
         final int numAddedStates = BreakdownUITools.getNumAddedStates(cmd);
         final int transCount = numAddedStates + 1;
-        final double fromX = canvas.lookupNode(fromState.getId()).getX();
-        final double fromY = canvas.lookupNode(fromState.getId()).getY();
-        final double toX = canvas.lookupNode(toState.getId()).getX();
-        final double toY = canvas.lookupNode(toState.getId()).getY();
+        final double fromX = graph.lookupNode(fromState.getId()).getX();
+        final double fromY = graph.lookupNode(fromState.getId()).getY();
+        final double toX = graph.lookupNode(toState.getId()).getX();
+        final double toY = graph.lookupNode(toState.getId()).getY();
 
         final double midPointX = 0.5 * (fromX + toX);
         final double midPointY = 0.5 * (fromY + toY);
@@ -263,14 +168,13 @@ public class BreakdownUITools {
         double dxPerNode = (toX - fromX) / transCount;
         double dyPerNode = (toY - fromY) / transCount;
 
-        Point2D middlePoint = canvas
-                .lookupEdge(cmd.getOriginalTransition().getId())
-                .getEdgeMiddlePoint();
+        Point2D middlePoint = graph.lookupEdge(
+                cmd.getOriginalTransition().getId()).getEdgeMiddlePoint();
         Point2D offsetVec = middlePoint.subtract(midPointX, midPointY);
         double mag = offsetVec.magnitude();
         LOGGER.log(Level.FINER, "oldmag = " + mag);
-        if (mag != 0 && mag < MIN_BREAKDOWN_HEIGHT_PX) {
-            offsetVec = offsetVec.multiply(MIN_BREAKDOWN_HEIGHT_PX / mag);
+        if (mag != 0 && mag < MIN_BREAKDOWN_HEIGHT) {
+            offsetVec = offsetVec.multiply(MIN_BREAKDOWN_HEIGHT / mag);
             LOGGER.log(Level.FINER, "newmag = " + offsetVec.magnitude());
         }
 
@@ -284,16 +188,12 @@ public class BreakdownUITools {
             if (tmpCmd instanceof AddStateCommand) {
                 curX += dxPerNode;
                 curY += dyPerNode;
-                points[i] = new Point2D(curX, curY);
+                points[i] = variedPoint(curX, curY);
                 i++;
             }
         }
 
         return points;
-    }
-
-    public static Point2D randomPoint() {
-        return new Point2D(Math.random() * 800, Math.random() * 600);
     }
 
 }
