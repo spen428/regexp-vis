c6eb132d23e562f5fecfb4203f3363f21f0606f1
S.J.Pengelly <sp611@kent.ac.uk>
2016-03-14 15:04:00 +0000

    User guide browser

---
+++ regexp_vis/src/controller/UserGuideView.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+/**
+ * Based on the example at
+ * https://docs.oracle.com/javafx/2/webview/jfxpub-webview.htm
+ * 
+ * @author sp611
+ *
+ */
+public class UserGuideView extends Region {
+
+    public static final double WIDTH = 400;
+    public static final double HEIGHT = 600;
+
+    private final WebView browser;
+    private final WebEngine engine;
+
+    public UserGuideView() {
+        super();
+        this.browser = new WebView();
+        this.engine = this.browser.getEngine();
+        URL url = this.getClass().getResource("/userguide/index.html");
+        System.out.println(url);
+        this.engine.load(url.toExternalForm());
+        getChildren().add(this.browser);
+    }
+
+}
